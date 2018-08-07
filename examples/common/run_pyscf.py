84d4b450e298c1e3c1ec4c2ed5a73a425d3de693
Michael F. Herbst <info@michael-herbst.com>
2018-08-07 18:18:37 +0200

    Script to run pyscf

---
+++ examples/common/run_pyscf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import pyscf
+import h5py
+import sys
+import numpy as np
+
+if len(sys.argv) != 4:
+    raise SystemExit("Need args: hdf5 integrals file, basis set, restricted")
+ifile = sys.argv[1]
+bas = sys.argv[2]
+
+if sys.argv[3] == "true":
+    restricted = True
+elif sys.argv[3] == "false":
+    restricted = False
+else:
+    raise SystemExit("restricted == true|false")
+
+with h5py.File(ifile, "r") as h5f:
+    nelec = list(h5f["system/nelec"])
+    atom_numbers = np.array(h5f["system/atom_numbers"])
+    coords = np.array(h5f["system/coords"])
+
+mol = pyscf.gto.Mole()
+mol.unit = "Bohr"
+mol.atom = [(str(atom_numbers[i]), coords[i]) for i in range(len(atom_numbers))]
+mol.nelec = nelec
+mol.basis = bas
+mol.verbose = 4
+mol.build()
+
+if mol.nelectron == 1 or mol.spin == 0:
+    mf = pyscf.scf.RHF(mol)
+    if not restricted:
+        raise SystemExit("Closed-shell systems are always restricded")
+else:
+    if restricted:
+        mf = pyscf.scf.ROHF(mol)
+    else:
+        mf = pyscf.scf.UHF(mol)
+mf.kernel()
