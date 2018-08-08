6ff53e48c5f15ce9e62ac7051c1b7ad47e7c7003
Michael F. Herbst <info@michael-herbst.com>
2018-08-08 17:16:20 +0200

    Adjust runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
-using SelfConsistentField
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
+@static if VERSION < v"0.7-"
+    const Test = Base.Test
 end
+
+using Test
+using SelfConsistentField
 
 include("functionality_hartee_fock.jl")
