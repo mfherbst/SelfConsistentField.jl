65b0293456431b9fa80964fd545b5e86bc715bab
Tobias Wackenhut <tobias@wackenhut.at>
2018-08-13 16:41:41 +0200

    move generic DIIS functions to separate file

--- src/algorithms/cDIIS.jl
+++ src/algorithms/cDIIS.jl
@@ -19,86 +19,12 @@ mutable struct cDIIS <: Accelerator
 end
 
 """
-    Computes next iterate using cDIIS
-"""
-function compute_next_iterate(acc::cDIIS, iterstate::ScfIterState)
-    # Push iterate and error to state
-    map(σ -> push_iterate!(acc.state[σ], spin(iterstate.fock, σ), spin(iterstate.error_pulay, σ)), spinloop(iterstate.fock))
-
-    # Check if the number of known fock and error matrices is equal for both
-    # spins before doing anything
-    history_size = acc.state[1].iterate.length
-    for σ in spinloop(iterstate.fock)
-        @assert acc.state[σ].iterate.length == history_size
-        @assert acc.state[σ].error.length == history_size
-    end
-
-    # To save memory we store only new_iterate once and pass views of it to the
-    # computation routines that write directly into the view.
-    new_iterate = zeros(size(iterstate.fock))
-
-    # Defining anonymous functions with given arguments improves readability later on.
-    matrix(σ) = diis_build_matrix(acc.state[σ])
-    coefficients(A) = diis_solve_coefficients(A, acc.conditioning_threshold)
-    compute(c, σ) = compute_next_iterate_matrix!(acc.state[σ], c, spin(new_iterate, σ), acc.coefficient_threshold)
-
-    # If sync_spins is enabled, we need to calculate the coefficients using the
-    # merged matrix. This also means we need to remove the same number of
-    # matrices from both histrories.
-    if acc.sync_spins & (spincount(iterstate.fock) == 2)
-        A = merge_matrices(matrix(1), matrix(2))
-        c, matrixpurgecount = coefficients(A)
-        map(σ -> compute(c, σ), spinloop(iterstate.fock))
-        map(σ -> purge_from_state(acc.state[σ], matrixpurgecount), spinloop(iterstate.fock))
-    else
-        # If we are calculating the spins separately, each spin has its own coefficients.
-        for σ in spinloop(iterstate.fock)
-            c, matrixpurgecount = coefficients(matrix(σ))
-            compute(c, σ)
-            purge_from_state(acc.state[σ], matrixpurgecount)
-        end
-    end
-    return update_iterate_matrix(iterstate, new_iterate)
-end
-
-"""
-    When synchronizing spins the resulting DIIS matrices have to be added
-    together but the constraint must be kept as is.
-"""
-function merge_matrices(A1::AbstractArray, A2::AbstractArray)
-    view(A1, 1:size(A1, 1) - 1, 1:size(A1, 2) - 1) .+ view(A2, 1:size(A2, 1) - 1, 1:size(A2, 2) - 1)
-    return A1
-end
-
-"""
-    Computes a new matrix to be used as Fock Matrix in next iteration
-    The function writes the result into the passed argument 'iterate'
-"""
-function compute_next_iterate_matrix!(state::DiisState, c::AbstractArray, iterate::SubArray, coefficient_threshold::Float64)
-    # add very small coefficients to the largest one but always use the most
-    # recent iterate matrix regardless of the coefficient value
-    mask = map(x -> norm(x) > coefficient_threshold, c)
-    mask[1] = true
-    c[argmax(c)] += sum(c[ .! mask])
-
-    # Construct new Fock Matrix using obtained coefficients
-    # and write it to the given iterate matrix. We assume, that
-    # iterate is a matrix of zeros.
-    for i in eachindex(c)[mask]
-        iterate .+= c[i] * state.iterate[i]
-    end
-end
-
-"""
     Solves the linear system after removing small eigenvalues to improve
     consistency.
-
-    Returns the vector c and and a boolean value representing if the matrix A
-    is so badly conditioned, that the previous fock matrix should be used.
 """
-function diis_solve_coefficients(A::AbstractArray, threshold::Float64)
+function diis_solve_coefficients(::cDIIS, A::AbstractArray, threshold::Float64)
     # Right hand side of the equation
-    rhs = diis_build_rhs(size(A, 1))
+    rhs = cdiis_build_rhs(size(A, 1))
 
     # calculate the eigenvalues of A and select sufficiently large eigenvalues
     λ, U = eigen(A)
@@ -129,14 +55,14 @@ function diis_solve_coefficients(A::AbstractArray, threshold::Float64)
 end
 
 """
-    Linear System Matrix for the cDIIM accelerator.
+    Linear System Matrix for the cDIIS accelerator.
     This is a hermitian matrix containing error overlaps B
     and ones in the form
     
     A = B  1
         1† 0
 """
-function diis_build_matrix(state::DiisState)
+function diis_build_matrix(::cDiis, state::DiisState)
     @assert state.n_diis_size > 0
     @assert state.iterate.length > 0
 
@@ -215,7 +141,7 @@ end
     This is a vector of size m+1 containing only ones
     except for the last element which is zero.
 """
-function diis_build_rhs(vectorsize::Int)
+function cdiis_build_rhs(vectorsize::Int)
     rhs = zeros(vectorsize)
     rhs[end] = 1
     return rhs
---
+++ src/parts/diis.jl
@@ -0,0 +1,71 @@
+"""
+    Computes next iterate using DIIS
+"""
+function compute_next_iterate(acc::cDIIS, iterstate::ScfIterState)
+    # Push iterate and error to state
+    map(σ -> push_iterate!(acc.state[σ], spin(iterstate.fock, σ), spin(iterstate.error_pulay, σ)), spinloop(iterstate.fock))
+
+    # Check if the number of known fock and error matrices is equal for both
+    # spins before doing anything
+    history_size = acc.state[1].iterate.length
+    for σ in spinloop(iterstate.fock)
+        @assert acc.state[σ].iterate.length == history_size
+        @assert acc.state[σ].error.length == history_size
+    end
+
+    # To save memory we store only new_iterate once and pass views of it to the
+    # computation routines that write directly into the view.
+    new_iterate = zeros(size(iterstate.fock))
+
+    # Defining anonymous functions with given arguments improves readability later on.
+    matrix(σ) = diis_build_matrix(acc, acc.state[σ])
+    coefficients(A) = diis_solve_coefficients(acc, A, acc.conditioning_threshold)
+    compute(c, σ) = compute_next_iterate_matrix!(acc.state[σ], c, spin(new_iterate, σ), acc.coefficient_threshold)
+
+    # If sync_spins is enabled, we need to calculate the coefficients using the
+    # merged matrix. This also means we need to remove the same number of
+    # matrices from both histrories.
+    if acc.sync_spins & (spincount(iterstate.fock) == 2)
+        A = merge_matrices(matrix(1), matrix(2))
+        c, matrixpurgecount = coefficients(A)
+        map(σ -> compute(c, σ), spinloop(iterstate.fock))
+        map(σ -> purge_from_state(acc.state[σ], matrixpurgecount), spinloop(iterstate.fock))
+    else
+        # If we are calculating the spins separately, each spin has its own coefficients.
+        for σ in spinloop(iterstate.fock)
+            c, matrixpurgecount = coefficients(matrix(σ))
+            compute(c, σ)
+            purge_from_state(acc.state[σ], matrixpurgecount)
+        end
+    end
+    return update_iterate_matrix(iterstate, new_iterate)
+end
+
+"""
+    When synchronizing spins the resulting DIIS matrices have to be added
+    together but the constraint must be kept as is.
+"""
+function merge_matrices(A1::AbstractArray, A2::AbstractArray)
+    view(A1, 1:size(A1, 1) - 1, 1:size(A1, 2) - 1) .+ view(A2, 1:size(A2, 1) - 1, 1:size(A2, 2) - 1)
+    return A1
+end
+
+"""
+    Computes a new matrix to be used as Fock Matrix in next iteration
+    The function writes the result into the passed argument 'iterate'
+"""
+function compute_next_iterate_matrix!(state::DiisState, c::AbstractArray, iterate::SubArray, coefficient_threshold::Float64)
+    # add very small coefficients to the largest one but always use the most
+    # recent iterate matrix regardless of the coefficient value
+    mask = map(x -> norm(x) > coefficient_threshold, c)
+    mask[1] = true
+    c[argmax(c)] += sum(c[ .! mask])
+
+    # Construct new Fock Matrix using obtained coefficients
+    # and write it to the given iterate matrix. We assume, that
+    # iterate is a matrix of zeros.
+    for i in eachindex(c)[mask]
+        iterate .+= c[i] * state.iterate[i]
+    end
+end
+
