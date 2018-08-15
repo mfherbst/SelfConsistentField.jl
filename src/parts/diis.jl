4c6f2f67b49849af84310e7a02aa77e33ddc00f9
Tobias Wackenhut <tobias@wackenhut.at>
2018-08-15 16:33:11 +0000

    fix matrix purging errors

--- src/parts/diis.jl
+++ src/parts/diis.jl
@@ -23,13 +23,13 @@ function compute_next_iterate(acc::Union{cDIIS,EDIIS}, iterstate::ScfIterState)
         A = merge_matrices(acc, matrix(1), matrix(2))
         c, matrixpurgecount = coefficients(A)
         map(σ -> compute(c, σ), spinloop(iterate))
-        map(σ -> purge_from_state(acc.state[σ], matrixpurgecount), spinloop(iterate))
+        map(σ -> purge_from_state(acc, acc.state[σ], matrixpurgecount), spinloop(iterate))
     else
         # If we are calculating the spins separately, each spin has its own coefficients.
         for σ in spinloop(iterate)
             c, matrixpurgecount = coefficients(matrix(σ))
             compute(c, σ)
-            purge_from_state(acc.state[σ], matrixpurgecount)
+            purge_from_state(acc, acc.state[σ], matrixpurgecount)
         end
     end
     return update_iterate_matrix(iterstate, new_iterate)
