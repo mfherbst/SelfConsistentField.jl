6b2bca754d27086c191f51acf2aa2ce45d92d2c5
Tobias Wackenhut <tobias@wackenhut.at>
2018-08-21 15:58:14 +0000

    migrate Roothan to new algorithm structure

--- src/algorithms/Roothaan.jl
+++ src/algorithms/Roothaan.jl
@@ -1,9 +1,11 @@
+struct Roothan end
+
 """
 Perform one step of the Roothaan algorithm if the Fock matrix is iterated
 """
-function roothan_step(problem::ScfProblem, iterate::FockIterState; kwargs...)
-    orben, orbcoeff = compute_orbitals(problem, iterate.fock; kwargs...)
-    density = compute_density(problem, orbcoeff)
-    fock, error_pulay, energies = compute_fock_matrix(problem, density; kwargs...)
+function Base.iterate(::Roothan, rp::SubReport)
+    orben, orbcoeff = compute_orbitals(rp.problem, rp.state.fock)
+    density = compute_density(rp.problem, orbcoeff)
+    fock, error_pulay, energies = compute_fock_matrix(rp.problem, density)
     return FockIterState(fock, error_pulay, energies, orbcoeff, orben, density)
 end
