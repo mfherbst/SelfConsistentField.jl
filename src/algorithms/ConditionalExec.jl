85267ab94cea9d10e5ab7e08e94873a40467e253
Tobias Wackenhut <tobias@wackenhut.at>
2018-08-22 13:54:05 +0000

    add ConditionalExec

---
+++ src/algorithms/ConditionalExec.jl
@@ -0,0 +1,12 @@
+mutable struct ConditionalExec <: Algorithm
+    algorithm::Algorithm
+    condition::Function
+end
+
+function initialize(ce::ConditionalExec, problem::ScfProblem, state::ScfIterState, softdefaults::Defaults)
+    initialize_if_neccessary(ce.algorithm, problem, state, softdefaults)
+end
+
+function iterate(ce::ConditionalExec, subreport::SubReport)
+    ce.condition(subreport) ? iterate(ce.algorithm, subreport) : nothing
+end
