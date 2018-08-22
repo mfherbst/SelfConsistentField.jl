d350cf5cdf5b0b013fa8510a9bdca73eedd9408e
Tobias Wackenhut <tobias@wackenhut.at>
2018-08-22 12:37:52 +0000

    fix log output

--- src/parts/reportmessage.jl
+++ src/parts/reportmessage.jl
@@ -8,8 +8,10 @@ function log!(rp::SubReport, msg::String, data::Any, level::Symbol...)
     if haskey(rp.loglevel, :stdout)
         if !isempty(level ∩ rp.loglevel[:stdout])
             print(level[end], ": ", msg, " ")
-            data != nothing && show(stdout, MIME("text/plain"), data)
-            println()
+            if data != nothing
+                show(stdout, MIME("text/plain"), data)
+                println()
+            end
         end
     end
     data
