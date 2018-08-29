e9f0b7a679f79b4f7432129839184b98961f7cf3
Tobias Wackenhut <tobias@wackenhut.at>
2018-08-29 11:04:23 +0000

    remove Report reference from SubReport

--- src/parts/initialize.jl
+++ src/parts/initialize.jl
@@ -22,7 +22,7 @@ function initialize(algorithm::Algorithm, problem::ScfProblem, guess_density::Ab
     # Construct report and add already existing log messages.
     pre_logger("setting up initial report", :debug,)
     report = Report(problem, initstate, missing, algorithm, Vector{SubReport}(), loglevel)
-    subreport = SubReport(Initializer(), problem, initstate, missing, logmessages, nothing, loglevel, report)
+    subreport = SubReport(Initializer(), problem, initstate, missing, logmessages, nothing, loglevel)
     push!(report.history, subreport)
 
     # log a fancy header
--- src/types/Report.jl
+++ src/types/Report.jl
@@ -9,21 +9,6 @@ mutable struct ReportMessage
 end
 ReportMessage(msg::String) = ReportMessage(msg, nothing)
 
-# There is a recursive dependency between a Report and Subreport. We break this
-# dependency by creating a constructor that automatically sets T as SubReport
-
-# In Julia 1.1 type redifinition should work. If you live in the future, please implement
-#    mutable struct SubReport end
-# and remove the current workaround
-mutable struct Report{T}
-    problem::ScfProblem
-    state::ScfIterState
-    convergence::Union{Missing, ScfConvergence}
-    algorithm::Algorithm
-    history::Vector{T}
-    loglevel::LogLevel
-end
-
 mutable struct SubReport
     algorithm::Union{Missing, Algorithm}
     problem::ScfProblem
@@ -32,9 +17,13 @@ mutable struct SubReport
     messages::Vector{ReportMessage}
     source::Union{Nothing, SubReport}
     loglevel::LogLevel
-    report::Report # There are cases in which algorithms need information about
-                   # past events and the whole Algorithm setup
 end
-# TODO remove reference to report in SubReport
 
-Report(p,s,c,a,h,l) = Report{SubReport}(p,s,c,a,h,l)
+mutable struct Report
+    problem::ScfProblem
+    state::ScfIterState
+    convergence::Union{Missing, ScfConvergence}
+    algorithm::Algorithm
+    history::Vector{SubReport}
+    loglevel::LogLevel
+end
