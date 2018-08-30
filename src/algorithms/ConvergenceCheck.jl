e345a25d2d7831aa77c99be72dfb02e36baaa112
Tobias Wackenhut <tobias@wackenhut.at>
2018-08-30 17:21:13 +0000

    migrate ScfConvergence to new structure and add some log output

--- src/algorithms/ConvergenceCheck.jl
+++ src/algorithms/ConvergenceCheck.jl
@@ -1,24 +1,34 @@
-mutable struct ConvergenceCheck <: Algorithm
+struct ConvergenceCheck <: Algorithm
     convergencecondition::Function
     olditerstate::Union{Missing, ScfIterState}
-    ConvergenceCheck(convergencecondition::Function) = new(convergencecondition, missing)
 end
 
-function ConvergenceCheck(; max_error_norm::Number = 5e-7, max_energy_total_change::Number = 1.25e-07, max_energy_1e_change::Number = 5e-5)
+function ConvergenceCheck(::ScfProblem, state::ScfIterState, lg::Logger, convergencecondition::Function; store_initial_matrix = true, params...)
+    ConvergenceCheck(convergencecondition, store_initial_matrix ? state : missing)
+end
+
+copy(cc::ConvergenceCheck) = ConvergenceCheck(cc.convergencecondition, cc.olditerstate)
+
+function ConvergenceCheck(problem::ScfProblem, state::ScfIterState, lg::Logger;
+                          max_error_norm::Number = 5e-7, max_energy_total_change::Number = 1.25e-07,
+                          max_energy_1e_change::Number = 5e-5, params...)
+
     function convergencecondition(convstate::ScfConvergence)
         (convstate.error_norm >= max_error_norm) |
         (convstate.energy_change["total"] >= max_energy_total_change) |
         (convstate.energy_change["1e"] >= max_energy_1e_change) ?
         false : true
     end
-    ConvergenceCheck(convergencecondition)
+    ConvergenceCheck(problem, state, lg, convergencecondition)
 end
 
-function iterate(cc::ConvergenceCheck, subreport::SubReport)
-    rp = new_subreport(subreport)
-    rp.state = subreport.state
+function iterate(cc::ConvergenceCheck, rp::SubReport)
+    newcc = ConvergenceCheck(cc.convergencecondition, rp.state)
+    lg = Logger(rp)
 
-    if !ismissing(cc.olditerstate)
+    if ismissing(cc.olditerstate)
+        return newcc, new_subreport(newcc, rp)
+    else
         error_norm = compute_error_norm(rp.problem, rp.state)
         energy_change = Dict{String, Float64}()
         for (key, oval) in cc.olditerstate.energies
@@ -27,11 +37,11 @@ function iterate(cc::ConvergenceCheck, subreport::SubReport)
             energy_change[key] = val
         end
 
-        rp.convergence = ScfConvergence(error_norm, energy_change, false)
-        rp.convergence.is_converged = cc.convergencecondition(rp.convergence)
-    end
+        updated_conv_data = ScfConvergence(error_norm, energy_change, false)
+        is_converged = cc.convergencecondition(updated_conv_data)
+        convergence = ScfConvergence(updated_conv_data.error_norm, updated_conv_data.energy_change, is_converged)
+        log!(lg, "new convergence", convergence, :info)
 
-    # Store new iterstate for next iteration
-    cc.olditerstate = rp.state
-    return cc, rp
+        return newcc, SubReport(newcc, rp.problem, rp.state, convergence, lg.messages, rp, rp.loglevel)
+    end
 end
