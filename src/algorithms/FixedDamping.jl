b64cfcd8c00bf9bf8765994792045c8faae43c57
Tobias Wackenhut <tobias@wackenhut.at>
2018-09-03 09:18:15 +0000

    migrate FixedDamping to new structure

--- src/algorithms/FixedDamping.jl
+++ src/algorithms/FixedDamping.jl
@@ -5,35 +5,22 @@ mutable struct FixedDamping <: Algorithm
     damping::Union{Missing, Float64}                   # Damping coefficient
 
     prev_iterate::Union{Nothing,ScfIterState}  # Previous iterate
-    function FixedDamping(; damping_value = missing)
-        new(damping_value, nothing)
-    end
 end
 
-function setup(fd::FixedDamping, problem::ScfProblem, iterstate::ScfIterState, params::Parameters)
-    # TODO needs to be implemented the same way in all other algorithms
-    damping = coalesce(fd.damping, get(params, :damping_value, missing), 0.4)
-    return FixedDamping(damping, fd.prev_iterate)
+function FixedDamping(problem::ScfProblem, iterstate::ScfIterState, lg::Logger; damping = 0.4, params...)
+    FixedDamping(damping, iterstate)
 end
 
-function iterate(fd::FixedDamping, subreport::SubReport)
-    rp = new_subreport(subreport)
-    rp.state = compute_next_iterate(fd, rp.source.state)
-    log!(rp, "inside FixedDamping", :debug)
-    return fd, rp
-end
+function iterate(fd::FixedDamping, rp::SubReport)
+    lg = Logger(rp)
+    log!(rp, "running FixedDamping", :debug)
 
-function compute_next_iterate(acc::FixedDamping, iterate::ScfIterState)
-    if acc.prev_iterate != nothing
-        prev_mat = get_iterate_matrix(acc.prev_iterate)
-        cur_mat = get_iterate_matrix(iterate)
-        damp = acc.damping
-        ret = (1 - damp) * prev_mat + damp * cur_mat
-        iterate = update_iterate_matrix(iterate, ret)
-        acc.prev_iterate = iterate
-        return iterate
-    else
-        acc.prev_iterate = iterate
-        return iterate
-    end
+
+    prev_mat = get_iterate_matrix(fd.prev_iterate)
+    cur_mat = get_iterate_matrix(rp.state)
+    ret = (1 - fd.damping) * prev_mat + fd.damping * cur_mat
+    iterate = update_iterate_matrix(rp.state, ret)
+
+    newdamping = FixedDamping(fd.damping, iterate)
+    return newdamping, new_subreport(newdamping, iterate, lg, rp)
 end
